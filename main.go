b8163bb7f4e468d1ba42ca513de97aad7505eb20
gregs <ukfaeku@gmail.com>
2022-12-14 18:59:47 -0300

    decode txs

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strings"
 
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -14,10 +18,7 @@ import (
 )
 
 var NodeEndpoint = ""
-
-func init() {
-	NodeEndpoint = os.Getenv("NODE_ENDPOINT")
-}
+var eth *ethclient.Client
 
 func ethClient() *ethclient.Client {
 	ethClient, err := ethclient.Dial(NodeEndpoint)
@@ -27,13 +28,21 @@ func ethClient() *ethclient.Client {
 	return ethClient
 }
 
+func init() {
+	NodeEndpoint = os.Getenv("NODE_ENDPOINT")
+	eth = ethClient()
+}
+
 var Routers = [3]common.Address{
 	common.HexToAddress("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"), // uniswap v2
-	common.HexToAddress("0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8"), // uniswap v3
+	// common.HexToAddress("0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8"), // uniswap v3
 	common.HexToAddress("0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F"), // sushiswap
 }
 
 func isRouter(address *common.Address) bool {
+	if address == nil {
+		return false
+	}
 	for _, router := range Routers {
 		if router == *address {
 			return true
@@ -59,8 +68,51 @@ func pendingTransactionsChannel() chan common.Hash {
 	return txnsHash
 }
 
+func GetLocalABI(path string) string {
+	abiFile, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer abiFile.Close()
+
+	result, err := io.ReadAll(abiFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(result)
+}
+
+func DecodeTransactionInputData(contractABI *abi.ABI, data []byte) {
+	// The first 4 bytes of the t represent the ID of the method in the ABI
+	// https://docs.soliditylang.org/en/v0.5.3/abi-spec.html#function-selector
+	methodSigData := data[:4]
+	method, err := contractABI.MethodById(methodSigData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inputsSigData := data[4:]
+	inputsMap := make(map[string]interface {
+	})
+	if err := method.Inputs.UnpackIntoMap(inputsMap, inputsSigData); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Method Name: %s\n", method.Name)
+	inputs, _ := json.Marshal(inputsMap)
+	fmt.Printf("Method inputs: %v\n", string(inputs))
+}
+
 func handleRouter(txn *types.Transaction) {
-	fmt.Println("Router:", txn.Hash())
+	fmt.Println("Router:", txn.To())
+	fmt.Println("tx hash:", txn.Hash())
+
+	contractABI, err := abi.JSON(strings.NewReader(GetLocalABI("abis/UniswapV2Router.json")))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DecodeTransactionInputData(&contractABI, txn.Data())
 }
 
 func main() {
